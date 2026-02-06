e70e1f8bb6ade7291e2b53671bcec6ed1f2ae86f
OlehHaliuk <oleh.haliuk@student.nhlstenden.com>
2026-02-06 09:47:07 +0100

    Changed delay to millis in blinks

--- Oleh/Blink_fast_or_slow.ino
+++ Oleh/Blink_fast_or_slow.ino
@@ -1,36 +1,28 @@
+unsigned long startMillis;  
+unsigned long currentMillis;
+unsigned long period = 1000;
 int redLed = 11;
 int buttonFast = 6;  
 int buttonSlow = 7;      
-int mode = 0;
        
 void setup(){
   pinMode(redLed, OUTPUT);
   pinMode(buttonFast, INPUT);
   pinMode(buttonSlow, INPUT);
+  startMillis = millis();  //initial start time
 }
 
 void loop(){
-  if (digitalRead(buttonFast) == LOW) {
-    mode = 1;
+  currentMillis=millis();
+  if(currentMillis-startMillis >= period){
+    digitalWrite(redLed,!digitalRead(redLed));
+    startMillis=currentMillis;
   }
-  else if (digitalRead(buttonSlow) == LOW) {
-    mode = 2;
+  if(digitalRead(buttonFast)==LOW){
+    period=500;
   }
-
-  int waitTime;
-
-  if (mode == 0) {
-    waitTime = 1000;
-  }
-  else if (mode == 1) {
-    waitTime = 500;
-  }
-  else {
-    waitTime = 2000;
+  else if(digitalRead(buttonSlow)==LOW){
+    period=2000;
   }
 
-  digitalWrite(redLed, LOW);
-  delay(50);
-  digitalWrite(redLed, HIGH);
-  delay(waitTime);
 }
