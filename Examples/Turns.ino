0b7c045d42c95081d280a596c483ceee78c5dc51
JustinasLaaunikonis <justinas.launikonis@gmail.com>
2026-03-03 13:53:09 +0100

    Updated Turn Function

--- Examples/Turns.ino
+++ Examples/Turns.ino
@@ -9,13 +9,18 @@ const int ROTATION_SENSOR_RIGHT = 2;
 
 // ===== WHEEL CONSTANTS =====
 const float WHEEL_DIAMETER = 65.0;
+const float WHEEL_CIRCUMFERENCE = 205.0; // WHEEL_DIAMETER * PI
 const int PULSES_PER_ROTATION = 20;
 
 const float ROBOT_WIDTH = 1000.0;
+const float TURN_ARC_PER_DEGREE = 2.18;  // arc length per degree of turn
 
 // ===== ROTATION COUNTERS =====
 volatile long rotationCounterLeft = 0;
 volatile long rotationCounterRight = 0;
+
+// tracks accumulated error over time
+float errorIntegral = 0;
 
 void setup() {
   pinMode(MOTOR_LEFT_FORWARD, OUTPUT);
@@ -28,58 +33,44 @@ void setup() {
   pinMode(ROTATION_SENSOR_RIGHT, INPUT_PULLUP);  // Right on pin 2
 
   // RISING = trigger when signal goes from LOW to HIGH
-  // runs countLeft() every time the left wheel rotates
-  attachInterrupt(digitalPinToInterrupt(3), countLeft, RISING);
-  attachInterrupt(digitalPinToInterrupt(2), countRight, RISING);
+  // runs onLeftWheelPulse() every time the left wheel rotates
+  attachInterrupt(digitalPinToInterrupt(3), onLeftWheelPulse, RISING);
+  attachInterrupt(digitalPinToInterrupt(2), onRightWheelPulse, RISING);
 
   delay(2000);
 }
 
 void loop() {
   int baseSpeed = 200;
-  turn90(true, baseSpeed); // left 90
+  turn(-90, baseSpeed); // left 90
   delay(1000);
 
-  turn90(false, baseSpeed); // right 90
+  turn(180, baseSpeed); // right 180
 
   while (true);
 }
 
 
-void turn90(bool turnLeft, int baseSpeed) {
+void turn(int degrees, int baseSpeed) {
 
   rotationCounterLeft = 0;
   rotationCounterRight = 0;
 
-  // full circle = 2piR
-  float arcLength = (PI * ROBOT_WIDTH) / 16.0;  // 90 degrees
-  float wheelCircumference = PI * WHEEL_DIAMETER;
-
-  int targetPulses = (arcLength / wheelCircumference) * PULSES_PER_ROTATION;
+  // calculate arc length for the turn angle
+  float arcLength = TURN_ARC_PER_DEGREE * abs(degrees);
+  int targetPulses = (arcLength / WHEEL_CIRCUMFERENCE) * PULSES_PER_ROTATION;
 
   while (true) {
 
-    if (turnLeft) {
+    if (degrees < 0) {  // negative = turn left
       if (rotationCounterRight >= targetPulses) break;
-
-      // left wheel stopped
-      analogWrite(MOTOR_LEFT_FORWARD, 0);
-      analogWrite(MOTOR_LEFT_BACK, 0);
-
-      // right wheel forward
-      analogWrite(MOTOR_RIGHT_FORWARD, baseSpeed);
-      analogWrite(MOTOR_RIGHT_BACK, 0);
+      setLeftMotor(0);
+      setRightMotor(baseSpeed);
     }
-    else {
+    else {  // positive = turn right
       if (rotationCounterLeft >= targetPulses) break;
-
-      // right wheel stopped
-      analogWrite(MOTOR_RIGHT_FORWARD, 0);
-      analogWrite(MOTOR_RIGHT_BACK, 0);
-
-      // left wheel forward
-      analogWrite(MOTOR_LEFT_FORWARD, baseSpeed);
-      analogWrite(MOTOR_LEFT_BACK, 0);
+      setRightMotor(0);
+      setLeftMotor(baseSpeed);
     }
   }
 
@@ -90,17 +81,35 @@ void turn90(bool turnLeft, int baseSpeed) {
 // ===== HELPER FUNCTIONS =====
 // These run automatically when encoders detect wheel rotation
 
-void countLeft() {
+void onLeftWheelPulse() {
   rotationCounterLeft++;
 }
 
-void countRight() {
+void onRightWheelPulse() {
   rotationCounterRight++;
 }
 
+void setLeftMotor(int speed) {
+  if (speed >= 0) {
+    analogWrite(MOTOR_LEFT_FORWARD, speed);
+    analogWrite(MOTOR_LEFT_BACK, 0);
+  } else {
+    analogWrite(MOTOR_LEFT_FORWARD, 0);
+    analogWrite(MOTOR_LEFT_BACK, -speed);
+  }
+}
+
+void setRightMotor(int speed) {
+  if (speed >= 0) {
+    analogWrite(MOTOR_RIGHT_FORWARD, speed);
+    analogWrite(MOTOR_RIGHT_BACK, 0);
+  } else {
+    analogWrite(MOTOR_RIGHT_FORWARD, 0);
+    analogWrite(MOTOR_RIGHT_BACK, -speed);
+  }
+}
+
 void stopMotors() {
-  analogWrite(MOTOR_LEFT_FORWARD, 0);
-  analogWrite(MOTOR_LEFT_BACK, 0);
-  analogWrite(MOTOR_RIGHT_FORWARD, 0);
-  analogWrite(MOTOR_RIGHT_BACK, 0);
+  setLeftMotor(0);
+  setRightMotor(0);
 }
